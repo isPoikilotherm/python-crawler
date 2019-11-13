f1d25467936d923fad732806f3cc2ee49ea00710
isPoikilotherm <18832026962@163.com>
2019-11-13 15:26:53 +0800

    ddd

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
-import tornado.ioloop
-import tornado.web
+from urllib import request,parse
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello,World")
-
-def make_app():
-        return tornado.web.Application([
-            (r"/",MainHandler),
-        ])
-if __name__ == '__main__':
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+url = 'https://httpbin.org/post'
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0',
+#     'Host': 'httpbin.org'
+# }
+dict = {
+    'name':'Germey'
+}
+data = bytes(parse.urlencode(dict),encoding='utf-8')
+req = request.Request(url=url,data=data,method='POST')
+req.add_header('User-Agent','Mozilla/5.0 (X11; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0')
+response = request.urlopen(req)
+print(response.read().decode('utf-8'))
+# print(response.status)
+# print(response.getheaders())
+# print(response.getheader('Server'))
