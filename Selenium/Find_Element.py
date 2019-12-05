17e1256f9285d826fbcd234b8f31414b1340a540
isPoikilotherm <18832026962@163.com>
2019-12-05 13:39:20 +0800

    ddd

--- Selenium/Find_Element.py
+++ Selenium/Find_Element.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 browser=webdriver.Chrome()
 browser.get('https://www.taobao.com')
 
--- Selenium/JavaScript.py
+++ Selenium/JavaScript.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 
+
 browser=webdriver.Chrome()
 browser.get('https://www.zhihu.com/explore')
 browser.execute_script('window.scrollTo(0,document.body.scrollHeight)')#滑轮到页面底部
