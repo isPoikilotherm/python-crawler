8e524335e76ae142c4a895d862f082012247af3e
isPoikilotherm <18832026962@163.com>
2020-01-12 16:19:15 +0800

    ddd

---
+++ Splash/render_png.py
@@ -0,0 +1,6 @@
+import requests
+
+url='http://localhost:8050/render.png?url=https:www.jd.com&wait=5&width=1000&height=700'
+response=requests.get(url)
+with open('jd.png','wb') as f:
+    f.write(response.content)
