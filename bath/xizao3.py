1f56ca7eb135ffb07954839e4927f48f3ceb5e1b
isPoikilotherm <18832026962@163.com>
2020-06-16 20:23:42 +0800

    预约洗澡

--- bath/xizao3.py
+++ bath/xizao3.py
@@ -32,6 +32,7 @@ class YuYue():
                 tijiao = self.browser.find_element_by_xpath('//*[@id="book-status-list"]/tbody/tr[45]/td[2]/button')
                 self.browser.execute_script("arguments[0].click()", tijiao)
                 time.sleep(1)
+
                 print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
                 self.browser.close()
 
